84cad40a403ca85bd0eb01f5e540707d6b026f70
Lewis Jones <lewisgwynjones@gmail.com>
2023-03-30 20:47:03 +0000

    fix bug where submitting correct or incorrect answer was producing NAN

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,8 +51,8 @@ function checkAnswer() {
  */
 function calculateCorrectAnswer() {
 
-    let operand1 = parseInt(document.getElementById('operand1').innertext);
-    let operand2 = parseInt(document.getElementById('operand2').innertext);
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById('operator').innerText;
 
     if (operator === "+") {
